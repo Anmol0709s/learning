11d2411f261bf545c572dbd20405af4c35634e4e
Anmol0709s <anmolkumar07sept@gmail.com>
2022-01-05 03:50:16 -0800

    just begining

---
+++ gettingStarted/printZ.java
@@ -0,0 +1,15 @@
+package gettingStarted;
+import java.util.*;
+
+public class printZ {
+
+  public static void main(String[] args) {
+
+System.out.println("*****");
+System.out.println("   * ");
+System.out.println("  *  ");
+System.out.println(" *   ");
+System.out.println("*****");
+
+  }
+}
